394a7f8f4071844fb72ccd5a1fa5e90bcbd7c6ee
Yahaya Idi Mahaman Laouali <myahayaidi@gmail.com>
2026-09-20 16:56:19 +0200

    debug: ajout de la route de diagnostic API

--- portfolio-backend/routes/web.php
+++ portfolio-backend/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -6,13 +7,26 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route de diagnostic (temporaire)
+Route::get('/debug-api', function () {
+    return response()->json([
+        'laravel_version' => app()->version(),
+        'php_version' => phpversion(),
+        'api_file_exists' => file_exists(base_path('routes/api.php')),
+        'api_file_size' => file_exists(base_path('routes/api.php')) ? filesize(base_path('routes/api.php')) : 0,
+        'route_cache_exists' => file_exists(base_path('bootstrap/cache/routes-v7.php')),
+        'route_cache_size' => file_exists(base_path('bootstrap/cache/routes-v7.php')) ? filesize(base_path('bootstrap/cache/routes-v7.php')) : 0,
+        'api_routes' => collect(Route::getRoutes())
+            ->filter(fn($r) => str_starts_with($r->uri(), 'api/'))
+            ->map(fn($r) => $r->methods()[0] . ' ' . $r->uri())
+            ->values()
+            ->toArray(),
+        'all_routes_count' => count(Route::getRoutes()),
+    ]);
 });
